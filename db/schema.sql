c3786795b91e1f09c44537e5afd76bbf922fc649
github username <github email address>
2016-02-25 20:08:07 -0500

    Algorithm for seeing who is friends with whom works now

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE if EXISTS members CASCADE;
+DROP TABLE if EXISTS friends CASCADE;
+
+CREATE TABLE members (
+
+  members_id SERIAL UNIQUE PRIMARY KEY,
+  member_name text,
+  email VARCHAR(320),
+  birthday date,
+  password text
+
+);
+
+CREATE TABLE friends (
+
+    members_id INTEGER,
+    friend_id INTEGER
+
+);
---
+++ db/seed.sql
@@ -0,0 +1,19 @@
+-- Inserting some members
+INSERT INTO members
+(member_name, email, birthday  ) VALUES ('Adam', 'adamkriger@gmail.com', '06/07/1984' );
+
+INSERT INTO members
+(member_name, email, birthday  ) VALUES ('Ben', 'benkriger@gmail.com', '06/07/1984' );
+
+INSERT INTO members
+(member_name, email, birthday  ) VALUES ('Lindsay', 'lkrigs@gmail.com', '06/07/1984' );
+
+-- Inserting some friends
+INSERT INTO friends
+(members_id, friend_Id ) VALUES (1,2);
+
+INSERT INTO friends
+(members_id, friend_Id ) VALUES (2,3);
+
+INSERT INTO friends
+(members_id, friend_Id ) VALUES (1,3);
