0d24dec7266d9e776e623539d9e4eeade31302ff
github username <github email address>
2016-02-28 16:18:42 -0500

    saving changes prior to adding send messages functionality

--- routes/members.js
+++ routes/members.js
@@ -11,19 +11,19 @@ var db = require('../db/pg');
 router.get('/all', db.getMembers,  (req, res) => {
   console.log(req.session.user.members_id)
   console.log("res.members[0]: ", res.members[0]);
-  res.render('pages/members.html.ejs', {members: res.members, me: req.session.user.members_id});
+  res.render('pages/members.html.ejs', {members: res.members, me: req.session.user.members_id, user: req.session.user});
 });
 
 router.post('/all', db.addFriend,db.getMember, (req, res) => {
   req.method = 'get';
-  res.writeHead(302, {location: '/../../users/' + req.session.user.members_id});
+  res.writeHead(302, {location: '/../../users/' + req.session.user.members_id, user: req.session.user});
   res.end();
 });
 
 
 
 router.get('/:members_id', db.getMember, db.displayFriends, (req, res) => {
-  res.render('pages/show.ejs', {member: res.members[0], friends: res.friends});
+  res.render('pages/show.ejs', {member: res.members[0], friends: res.friends, user: req.session.user});
 });
 
 
