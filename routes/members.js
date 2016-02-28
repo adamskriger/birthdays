b0ca4ba95743d129d4bde752e032122e9ec49b7e
github username <github email address>
2016-02-28 09:09:42 -0500

    Can now add friends into database. Need to update HTML to reflect this.

--- routes/members.js
+++ routes/members.js
@@ -10,12 +10,14 @@ var db = require('../db/pg');
 
 router.get('/all', db.getMembers,  (req, res) => {
   console.log(req.session.user.members_id)
+  console.log("res.members[0]: ", res.members[0]);
   res.render('pages/members.html.ejs', {members: res.members, me: req.session.user.members_id});
 });
 
 router.post('/all', db.addFriend, (req, res) => {
-  // console.log("This is the post being hit ");
-  res.redirect('members/all',{members: res.members} );
+  console.log("res.members addFriend: ", res.members);
+
+  res.redirect('pages/members.html.ejs',{members: res.members} );
 });
 
 
