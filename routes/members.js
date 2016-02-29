f9dd3bab2b9cc23c224c19289b89cd91107af41f
github username <github email address>
2016-02-29 11:27:42 -0500

    Going official with amazon s3 upload module. This commit has the other npm package

--- routes/members.js
+++ routes/members.js
@@ -1,11 +1,55 @@
 // 'use strict'
-
+var multiparty = require('multiparty');
 var express = require('express');
 var router = express.Router();
 var db = require('../db/pg');
+var s3 = require('s3');
 
 
+var params = {
+  localFile: "files.avatar[0].path",
 
+  s3Params: {
+    Bucket: "birthdaysproject",
+    Key: "some/remote/file",
+    // other options supported by putObject, except Body and ContentLength.
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+  },
+};
+var uploader = client.uploadFile(params);
+uploader.on('error', function(err) {
+  console.error("unable to upload:", err.stack);
+});
+uploader.on('progress', function() {
+  console.log("progress", uploader.progressMd5Amount,
+            uploader.progressAmount, uploader.progressTotal);
+});
+uploader.on('end', function() {
+  console.log("done uploading");
+});
+// var papercut = require('papercut');
+
+// var papercut = require('papercut');
+//   papercut.configure('production', function(){
+//   papercut.set('storage', 's3')
+//   papercut.set('S3_KEY', process.env.S3_KEY)
+//   papercut.set('S3_SECRET', process.env.S3_SECRET)
+//   papercut.set('bucket', 'birthdaysproject')
+// });
+
+// var AvatarUploader = papercut.Schema(function(schema){
+//   schema.version({
+//     name: 'avatar',
+//     size: '200x200',
+//     process: 'crop'
+//   });
+//
+//   schema.version({
+//     name: 'small',
+//     size: '50x50',
+//     process: 'crop'
+//   });
+// });
 
 
 router.get('/all', db.getMembers,  (req, res) => {
@@ -20,6 +64,28 @@ router.post('/all', db.addFriend,db.getMember, (req, res) => {
   res.end();
 });
 
+// router.get('/avatar', (req, res) => {
+//   res.render('pages/avatar.html.ejs');
+// });
+//
+//
+// router.post ('/avatar', (req, res) => {
+//     uploader = new AvatarUploader();
+//     form = new multiparty.Form()
+//     form.parse(req, function(err, fields, files) {
+//       // eval(pry.it)
+//
+//       uploader.process(files.avatar[0].originalFilename, files.avatar[0].path, function(err, images){
+//             console.log(images);
+//             // {
+//             //  thumbnail: '/images/upload/412341-thumbnail.jpg',
+//             //  large: '/images/upload/412341-large.jpg',
+//             //  origin: '/images/upload/412341-origin.jpg'
+//             // }
+//           });
+//     })
+//     })
+
 
 
 router.get('/:members_id', db.getMember, db.displayFriends, (req, res) => {
@@ -32,6 +98,8 @@ router.post('/message', db.sendMessage, (req, res) => {
   req.method = 'get';
   res.writeHead(302, {location: '/../../users/' + req.session.user.members_id, user: req.session.user});
   res.end();});
+
+
 
 
 
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
-
+var dotenv = require('dotenv');
+pry = require('pryjs');
 var path = require('path');
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var papercut = require('papercut');
-
+// var papercut = require('papercut');
+// var papercut = require('./')
+// var async = require('async');
+// var knox = require('knox');
+// var imagemagick = require('imagemagick')
 ///Log-in Stuff Appears Below:
 var config = "postgres://Adam1:Move2core@localhost/birthdays";
 var pg = require('pg');
@@ -17,8 +20,19 @@ var app = express();
 var userRoutes = require( path.join(__dirname, '/routes/users'));
 var session = require('express-session');
 var pgSession = require('connect-pg-simple')(session);
-
 var membersRouter = require('./routes/members');
+var s3 = require('s3');
+
+//
+// var client = knox.createClient({
+//     key: 'AKIAJUHX7LZLFQ7FVRIA',
+//     secret: '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',
+//     bucket: 'birthdaysproject'
+// });
+
+// client.deleteFile('/birthdaysproject', function(err, res){
+//     console.log('Clip delete err :',err,', Clip path :', 'birthdaysproject/');
+// });
 
 dotenv.load();
 
@@ -42,7 +56,7 @@ app.use(morgan('dev'));
 
 app.use(methodOverride('_method'));
 
-app.use(express.static('./public/'));
+app.use(express.static(path.join(__dirname, './public/')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -51,6 +65,62 @@ app.use('/users', userRoutes)
 app.use('/members', membersRouter);
 
 
+var client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default
+  s3RetryCount: 3,    // this is the default
+  s3RetryDelay: 1000, // this is the default
+  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+  multipartUploadSize: 15728640, // this is the default (15 MB)
+  s3Options: {
+    accessKeyId: "AKIAJUHX7LZLFQ7FVRIA",
+    secretAccessKey: "R5UBez5Rw1DAczgL/qpBHuO63rOkcF6okCAY90TF",
+    region: "Oregon",
+    // endpoint: 's3.yourdomain.com',
+    // sslEnabled: false
+    // any other options are passed to new AWS.S3()
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+  },
+});
+
+
+// papercut
+// configure = (function(){
+//   papercut.set('storage', 'file')
+//   papercut.set('directory', './images/uploads')
+//   papercut.set('url', '/images/uploads')
+// });
+//
+// papercut = function(){
+//   papercut.set('storage', 's3')
+//   papercut.set('S3_KEY', process.env.S3_KEY)
+//   papercut.set('S3_SECRET', process.env.S3_SECRET)
+//   papercut.set('bucket', 'birthdaysproject')
+// };
+
+// eval(pry.it)
+
+// var AvatarUploader = papercut.Schema(function(schema){
+//   schema.version({
+//     name: 'avatar',
+//     size: '200x200',
+//     process: 'crop'
+//   });
+//
+//   schema.version({
+//     name: 'small',
+//     size: '50x50',
+//     process: 'crop'
+//   });
+// });
+//
+// var uploader = new AvatarUploader();
+
+// var pc = function(images){
+//   console.log(images.avatar); // '/images/uploads/image1-avatar.jpg'
+//   console.log(images.small); // '/images/uploads/image1-small.jpg'
+// }
+
+  ////papercut
 
 app.get('/', function(req, res) {
   console.log("LOG4: ", req.session);
